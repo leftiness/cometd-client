8816af54a16351e6463fca7d744877de749f0201
Brandon Parmenter <leftiness@gmail.com>
2021-02-03 00:53:22 -0500

    Closes #1: Gradle JavaFX hello world

---
+++ app/src/test/java/cometd/client/AppTest.java
@@ -0,0 +1,11 @@
+package cometd.client;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class AppTest {
+  @Test public void testAppHasAGreeting() {
+    App classUnderTest = new App();
+    assertNotNull("app is not null", classUnderTest);
+  }
+}
